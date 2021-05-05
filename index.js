1b553ddddeaf7a8cdca523d06a7bcf328f970c07
Tomas Shaurli <tshaurli@gmail.com>
2021-05-05 17:52:30 -0300

    as

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const app = express()
+const port = process.env.PORT || 5000
 
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+app.listen(port, () => {
+	console.log(`listenin on ${port}`)
+})
+/*
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/index'))	
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
