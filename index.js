42354da0855cbcc2a80ec0dfd040a5651845d255
TomasShaurli <50838177+TomasShaurli@users.noreply.github.com>
2021-05-05 20:18:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/test', (req, res) => {
-	res.send('Hello World!2')
+app.use(express.json())
+
+app.post('/', function (req, res) {
+  console.log(req.body)
+  res.json(req.body)
+  res.sendStatus(200)
 })
 app.listen(port, () => {
-	console.log(`listenin on ${port}`)
+	console.log(`listenin omm ${port}`)
 })
-/*
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))	
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-*/
