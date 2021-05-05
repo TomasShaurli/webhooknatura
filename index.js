d2120b1119d667d5bc6b80db031f680e86020de4
TomasShaurli <50838177+TomasShaurli@users.noreply.github.com>
2021-05-05 17:57:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
+app.get('/test', (req, res) => {
+	res.send('Hello World!2')
 })
 app.listen(port, () => {
 	console.log(`listenin on ${port}`)
